8c57b0be9c51a23229a464876db45717cde0192f
Douglas Webb <douglasdwebb@msn.com>
2020-01-29 18:28:33 -0500

    question population functional

--- pilotQuiz/pilotQuiz/views.py
+++ pilotQuiz/pilotQuiz/views.py
@@ -12,7 +12,7 @@ name in the function's return value.
 
 
 
-
+import random
 
 from django.shortcuts import render
 from question.models import Question
@@ -20,11 +20,31 @@ from question.forms import QuestionSelectForm
 
 def homePage(request):
 
-    selectedQuestion = request.GET ['selectedValue'];
-    print(selectedQuestion);
+
+
+    try:
+        selectedQuestion = request.GET ['selectedValue'];
+    except:
+        selectedQuestion = "L-1-1"
+
+
+    optionBank = []; #empty array to establsih option bank.
+
+
 
     form = QuestionSelectForm(request.POST)
     data = Question.objects;
     entry = Question.objects.get(QuestionReferenceCode = selectedQuestion)
 
-    return render(request, 'home.html', {'testvariable': 'this Works', 'entry': entry, 'data':data, 'form':form, 'Qselect':selectedQuestion})
+    optionBank.append((entry.answer,entry.answerDetails, True, 'Button1','selector1','descriptor1')) ## this appends the correct answer as the first item in the list.
+
+    optionBank.append((entry.herring1,entry.herringDetails1, False, 'Button2','selector2','descriptor2'))
+    optionBank.append((entry.herring2,entry.herringDetails2, False, 'Button3','selector3','descriptor3'))
+    optionBank.append((entry.herring3,entry.herringDetails3, False, 'Button4','selector4','descriptor4'))
+    optionBank.append((entry.herring4,entry.herringDetails4, False, 'Button5','selector5','descriptor5'))
+
+    random.shuffle(optionBank);
+
+
+
+    return render(request, 'home.html', {'optionBank': optionBank, 'testvariable': 'this Works', 'entry': entry, 'data':data, 'form':form, 'Qselect':selectedQuestion})
